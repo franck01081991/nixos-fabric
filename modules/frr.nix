bfed0cc84c693b196a068497f70000b5fc32d12d
Franck Laplagne <franck.sembin.apou@gmail.com>
2025-12-27 22:00:09 +0100

    fix: Complete rewrite of FRR module with proper syntax

--- modules/frr.nix
+++ modules/frr.nix
@@ -125,7 +125,7 @@ in {
         
         # BGP configuration
         ${lib.mkIf cfg.bgp.enable (lib.concatStringsSep "\n" (
-          ([
+          [
             "router bgp ${toString cfg.bgp.as}"
             "  bgp router-id ${cfg.bgp.routerId}"
           ]
@@ -139,8 +139,8 @@ in {
                  ++ [ "  exit-address-family" ]
                )
              ) cfg.bgp.addressFamilies)
-          ++ [ "!" ])
-        )}
+          ++ [ "!" ]
+        ))}
         
         # EVPN configuration
         ${lib.mkIf cfg.evpn.enable (lib.concatStringsSep "\n" [
@@ -163,17 +163,159 @@ in {
     };
     
     # Firewall rules for FRR (BGP port 179)
-    networking.nftables.ruleset = lib.concatStringsSep "\n" [
-      (if cfg.security.enable then ''
-        table inet filter {
-          chain input {
-            # Allow BGP on WireGuard interface
-            iifname "wgtransport" tcp dport 179 accept comment "Allow BGP on WireGuard"
-            # Allow OSPF multicast
-            udp dport { 89 520 } accept comment "Allow OSPF multicast"
-          }
-        }
-      '' else "")
-    ];
+    networking.nftables.ruleset = lib.mkIf cfg.security.enable (lib.concatStringsSep "\n" [
+      "table inet filter {"
+      "  chain input {"
+      "    # Allow BGP on WireGuard interface"
+      "    iifname \"wgtransport\" tcp dport 179 accept comment \"Allow BGP on WireGuard\""
+      "    # Allow OSPF multicast"
+      "    udp dport { 89 520 } accept comment \"Allow OSPF multicast\""
+      "  }"
+      "}"
+    ]);
   };
-}
+
+  in {
+    options.network-fabric.frr = {
+      enable = lib.mkDefault false;
+      
+      # Security settings for FRR
+      security = lib.mkOption {
+        type = lib.types.submodule {
+          options = {
+            enable = lib.mkDefault true;
+            # BGP security - RFC 8205 (BGPsec) and RFC 7454 (BGP Operations and Security)
+            bgpMaxPrefix = lib.mkDefault 1000;
+            bgpTtlSecurity = lib.mkDefault true;
+            bgpPrefixList = lib.mkDefault [];
+            # OSPF security
+            ospfAuthentication = lib.mkDefault false;
+            ospfAuthenticationKey = lib.mkDefault "";
+          };
+        };
+      };
+      
+      bgp = lib.mkOption {
+        type = lib.types.submodule {
+          options = {
+            enable = lib.mkDefault false;
+            as = lib.mkDefault 65000;
+            routerId = lib.mkDefault "10.254.0.1";
+            neighbors = lib.mkDefault {};
+            networks = lib.mkDefault [];
+            addressFamilies = lib.mkDefault [ "ipv4 unicast" ];
+          };
+        };
+      };
+
+      ospf = lib.mkOption {
+        type = lib.types.submodule {
+          options = {
+            enable = lib.mkDefault false;
+            routerId = lib.mkDefault "10.254.0.1";
+            area = lib.mkDefault 0;
+            networks = lib.mkDefault [];
+            passiveInterfaces = lib.mkDefault [ "default" ];
+          };
+        };
+      };
+
+      evpn = lib.mkOption {
+        type = lib.types.submodule {
+          options = {
+            enable = lib.mkDefault false;
+            neighbors = lib.mkDefault [];
+          };
+        };
+      };
+    };
+
+    config = lib.mkIf cfg.enable {
+      services.frr = {
+        enable = true;
+        bgpd.enable = cfg.bgp.enable;
+        ospfd.enable = cfg.ospf.enable;
+        
+        config = lib.concatStringsSep "\n" [
+          "frr defaults traditional"
+          "hostname ${config.networking.hostName}"
+          "service integrated-vtysh-config"
+          "log syslog informational"
+          
+          # Security configuration
+          ${lib.mkIf cfg.security.enable (lib.concatStringsSep "\n" (
+            [
+              "!"
+              "! BGP Security - RFC 8205 (BGPsec) and RFC 7454 (BGP Operations and Security)"
+              "!"
+            ]
+            ++ (lib.mkIf cfg.security.bgpTtlSecurity [
+              "!"
+              "! BGP TTL Security - RFC 5082"
+              "!"
+              "access-list BGP_TTL_SECURITY permit any"
+              "!"
+            ] [])
+            ++ (lib.mkIf (cfg.security.bgpMaxPrefix != null && cfg.security.bgpMaxPrefix > 0) (
+              [
+                "!"
+                "! BGP Prefix Limit - RFC 7606"
+                "!"
+              ]
+              ++ (lib.map (neighborName: 
+                "neighbor ${(builtins.attrValues cfg.bgp.neighbors)[0].ip} maximum-prefix ${toString cfg.security.bgpMaxPrefix} warning-threshold 80 restart 60"
+              ) (builtins.attrNames cfg.bgp.neighbors))
+              ++ [ "!" ]
+            ) [])
+            ++ (lib.mkIf cfg.security.ospfAuthentication [
+              "!"
+              "! OSPF Authentication"
+              "!"
+              "interface ${lib.concatStringsSep " " (lib.map (network: lib.stringReplace "/.*" "" network) cfg.ospf.networks)}"
+              "  ip ospf authentication message-digest"
+              "  ip ospf message-digest-key 1 md5 ${cfg.security.ospfAuthenticationKey}"
+              "!"
+            ] [])
+          ))}
+          
+          # BGP configuration
+          ${lib.mkIf cfg.bgp.enable (lib.concatStringsSep "\n" (
+            [
+              "router bgp ${toString cfg.bgp.as}"
+              "  bgp router-id ${cfg.bgp.routerId}"
+            ]
+            ++ (if cfg.bgp.clusterId then [ "  bgp cluster-id ${cfg.bgp.clusterId}" ] else [])
+            ++ [ generateBGPNeighbors cfg.bgp.neighbors ]
+            ++ (lib.map (af:
+                 lib.concatStringsSep "\n" (
+                   [ "  address-family ${af}" ]
+                   ++ (lib.map (network: "    network ${network}") cfg.bgp.networks)
+                   ++ [ generateBGPNeighbors (lib.filterAttrs (name: neighbor: neighbor.addressFamilies && lib.elem af neighbor.addressFamilies) cfg.bgp.neighbors) ]
+                   ++ [ "  exit-address-family" ]
+                 )
+               ) cfg.bgp.addressFamilies)
+            ++ [ "!" ]
+          ))}
+          
+          # EVPN configuration
+          ${lib.mkIf cfg.evpn.enable (lib.concatStringsSep "\n" [
+            "router bgp ${toString cfg.bgp.as} vrf default"
+            "  address-family l2vpn evpn"
+            "    neighbor ${lib.concatStringsSep " " cfg.evpn.neighbors} activate"
+            "  exit-address-family"
+            "!"
+          ])}
+          
+          # OSPF configuration
+          ${lib.mkIf cfg.ospf.enable (lib.concatStringsSep "\n" [
+            "router ospf"
+            "  ospf router-id ${cfg.ospf.routerId}"
+            generateOSPFNetworks cfg.ospf.networks
+            lib.concatStringsSep "\n" (lib.map (iface: "  passive-interface ${iface}") cfg.ospf.passiveInterfaces)
+            "!"
+          ])}
+        ];
+      };
+    };
+  };
+};
